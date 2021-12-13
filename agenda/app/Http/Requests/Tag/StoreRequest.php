5d86edc4ab354316db8d0eef29cab0539e42a1cd
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-12-13 10:51:18 -0300

    Criaçao da tag do usuario

---
+++ agenda/app/Http/Requests/Tag/StoreRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests\Tag;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     *  Filters to be applied to the input.
+     *
+     *  @return array
+     */
+    public function filters()
+    {
+        return [
+            'after' => [
+                'description' => 'cast:string',
+            ],
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'description' => [
+                'required',
+                Rule::unique('tags')->where('user_id', user('id'))
+            ],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'description.required' => 'O campo DESCRIÇÃO é obrigatório',
+            'description.unique'   => 'Já existe uma tag com este nome.',
+        ];
+    }
+}
