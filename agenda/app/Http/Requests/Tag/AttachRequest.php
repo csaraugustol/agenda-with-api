dead602739cff2e2a0f11eb879b3f3e55f5b25dc
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-12-14 15:22:04 -0300

    Vinculo de tag e contato

---
+++ agenda/app/Http/Requests/Tag/AttachRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\Traits\SanitizesInput;
+
+class AttachRequest extends FormRequest
+{
+    use SanitizesInput;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     *  Filters to be applied to the input.
+     *
+     *  @return array
+     */
+    public function filters()
+    {
+        return [
+            'after' => [
+                'contact_id' => 'cast:string',
+            ],
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'contact_id' => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'contact_id.required' => 'O campo ID DO CONTATO é obrigatório',
+        ];
+    }
+}
---
+++ agenda/app/Http/Resources/TagContact/TagContactResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources\TagContact;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TagContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'         => $this->resource->id,
+            'tag_id'     => $this->resource->tag_id,
+            'contact_id' => $this->resource->contact_id,
+        ];
+    }
+}
