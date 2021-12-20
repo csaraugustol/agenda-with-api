5c569a60b836f8690f3e68e93faaf2ba72a302b4
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-12-20 14:27:27 -0300

    Alinhamento metodos

--- agenda/app/Services/Contracts/ContactServiceInterface.php
+++ agenda/app/Services/Contracts/ContactServiceInterface.php
@@ -8,7 +8,7 @@ use App\Services\Params\Contact\CreateCompleteContactsServiceParams;
 interface ContactServiceInterface
 {
     public function find(string $contactId): ServiceResponse;
+    public function store(CreateCompleteContactsServiceParams $params): ServiceResponse;
     public function findByUserContact(string $userId, string $contactId): ServiceResponse;
     public function findAllWithFilter(string $userId, string $filter = null): ServiceResponse;
-    public function store(CreateCompleteContactsServiceParams $params): ServiceResponse;
 }
