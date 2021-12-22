b56ff5152c00c4c5b583b3e5661d354d92482454
Cesar Augusto <cesar.leitejm@hotmail.com>
2021-12-22 11:40:53 -0300

    Ajuste na mensagem

--- agenda/app/Services/AddressService.php
+++ agenda/app/Services/AddressService.php
@@ -111,11 +111,11 @@ class AddressService extends BaseService implements AddressServiceInterface
             if (is_null($findContactResponse->data)) {
                 return new ServiceResponse(
                     false,
-                    'Não é possível realizar a atualização do endereço.',
+                    'O contato não foi encontrado.',
                     null,
                     [
                         new InternalError(
-                            'Não é possível realizar a atualização do endereço.',
+                            'O contato não foi encontrado.',
                             15
                         )
                     ]
