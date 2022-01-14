5f078f6ecc4850e906d0f3f708b1a67ecf1a9895
Cesar Augusto <cesar.leitejm@hotmail.com>
2022-01-14 10:36:54 -0300

    Testes de relacionamento

--- agenda/tests/Integration/Models/TagTest.php
+++ agenda/tests/Integration/Models/TagTest.php
@@ -4,17 +4,32 @@ namespace Tests\Integration\Models;
 
 use App\Models\Tag;
 use App\Models\User;
+use App\Models\TagContact;
 use Tests\Unit\Services\BaseTestCase;
+use Illuminate\Database\Eloquent\Collection;
 
 class TagTest extends BaseTestCase
 {
     /**
-     * Verifica o relacionamento com a Tag
+     * Verifica o relacionamento da Tag com o User
      */
     public function testRelationshipTagWithUser()
     {
         $tag = factory(Tag::class)->create();
 
         $this->assertInstanceOf(User::class, $tag->user);
+    }
+
+    /**
+     * Verifica o relacionamento da Tag com a TagContacts
+     */
+    public function testRelationshipTagWithTagContact()
+    {
+        $tag = factory(Tag::class)->create();
+
+        factory(TagContact::class)->create(['tag_id' => $tag->id]);
+
+        $this->assertInstanceOf(Collection::class, $tag->tagContacts);
+        $this->assertInstanceOf(TagContact::class, $tag->tagContacts->first());
     }
 }
