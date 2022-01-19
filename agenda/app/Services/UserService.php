712f6a34abf44f1eaa0065d7ccdb3cc9cae4e68c
Cesar Augusto <cesar.leitejm@hotmail.com>
2022-01-18 22:48:24 -0300

    Ajuste

--- agenda/app/Services/UserService.php
+++ agenda/app/Services/UserService.php
@@ -84,7 +84,6 @@ class UserService extends BaseService implements UserServiceInterface
             }
 
             $user = $findUserResponse->data;
-            //dd($password, $user->password);
             if (!password_verify($password, $user->password)) {
                 return new ServiceResponse(
                     false,
