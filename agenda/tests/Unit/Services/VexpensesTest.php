8949121caf56b825656af5726efef7263602b800
Cesar Augusto <cesar.leitejm@hotmail.com>
2022-02-17 20:54:26 -0300

    Testes para verificar integracao e filtragens

--- agenda/tests/Unit/Services/VexpensesTest.php
+++ agenda/tests/Unit/Services/VexpensesTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Unit\Services;
 
+use App\Models\Contact;
 use App\Models\User;
 use App\Models\ExternalToken;
 use App\Services\Responses\ServiceResponse;
 use Tests\Mocks\Providers\VexpensesProvider;
 use App\Services\Contracts\VexpensesServiceInterface;
+use App\Services\VexpensesService;
 
 class VexpensesTest extends BaseTestCase
 {
@@ -141,5 +143,101 @@ class VexpensesTest extends BaseTestCase
         $this->assertNotTrue($listMembersResponse->success);
         $this->assertNull($listMembersResponse->data);
         $this->assertHasInternalError($listMembersResponse, 24);
+    }
+
+    /**
+     * Retorna sucesso ao listar apenas os membros da API que possuem telefone.
+     * Foram criados dois contatos e o sucesso deve retornar apenas um, fazendo
+     * a filtragem e removendo o membro que não possui telefone
+     */
+    public function testReturSuccessWhenListMembersAndFilterJustMemberWithPhone()
+    {
+        $user = factory(User::class)->create();
+
+        factory(ExternalToken::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $mockMembersResponse = $this->vexpensesProvider
+            ->getMockReturnMemberWithPhoneAndOtherMemberWithoutPhone();
+
+        $this->vexpensesProvider->setMockRequest(
+            $this->vexpensesService,
+            $mockMembersResponse->status_code,
+            $mockMembersResponse->response,
+        );
+
+        $listMembersResponse = $this->vexpensesService->findAllTeamMembers($user->id);
+
+        $this->assertInstanceOf(ServiceResponse::class, $listMembersResponse);
+        $this->assertTrue($listMembersResponse->success);
+        $this->assertNotNull($listMembersResponse->data);
+        $this->assertEquals(1, count($listMembersResponse->data));
+        $this->assertEquals(
+            $listMembersResponse->data->first()->phones[0],
+            $mockMembersResponse->response->data[0]->phone1
+        );
+    }
+
+    /**
+     * Retorna sucesso ao lista um membro que não possui integração
+     */
+    public function testReturSuccessWhenListMemberWithoutIntegration()
+    {
+        $user = factory(User::class)->create();
+
+        $contact = factory(Contact::class)->create();
+
+        factory(ExternalToken::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $mockMembersResponse = $this->vexpensesProvider->getMockReturnAllMembers();
+
+        $this->vexpensesProvider->setMockRequest(
+            $this->vexpensesService,
+            $mockMembersResponse->status_code,
+            $mockMembersResponse->response,
+        );
+
+        $listMembersResponse = $this->vexpensesService->findAllTeamMembers($user->id);
+
+        $this->assertInstanceOf(ServiceResponse::class, $listMembersResponse);
+        $this->assertTrue($listMembersResponse->success);
+        $this->assertNotNull($listMembersResponse->data);
+        $this->assertNotEquals($listMembersResponse->data->first()->external_id, $contact->external_id);
+    }
+
+    /**
+     * Retorna sucesso ao lista um membro que não possui integração
+     */
+    public function testReturSuccessWhenListMemberWithIntegration()
+    {
+        $externalId = $this->faker->numberBetween(1, 100);
+        $user = factory(User::class)->create();
+
+        $contact = factory(Contact::class)->create([
+            'external_id' => $externalId,
+        ]);
+
+        factory(ExternalToken::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $mockMembersResponse = $this->vexpensesProvider
+            ->getMockReturnAllMembers($contact->external_id);
+
+        $this->vexpensesProvider->setMockRequest(
+            $this->vexpensesService,
+            $mockMembersResponse->status_code,
+            $mockMembersResponse->response,
+        );
+
+        $listMembersResponse = $this->vexpensesService->findAllTeamMembers($user->id);
+
+        $this->assertInstanceOf(ServiceResponse::class, $listMembersResponse);
+        $this->assertTrue($listMembersResponse->success);
+        $this->assertNotNull($listMembersResponse->data);
+        $this->assertEquals($listMembersResponse->data->first()->external_id, $contact->external_id);
     }
 }
