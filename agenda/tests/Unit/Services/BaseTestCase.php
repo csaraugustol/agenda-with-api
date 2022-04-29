61e441341187099ec50b00d500ee02f81d50a147
Cesar Augusto <cesar.leitejm@hotmail.com>
2022-04-29 11:47:06 -0300

    Teste para tentar cobrir a camada catch

--- agenda/tests/Unit/Services/BaseTestCase.php
+++ agenda/tests/Unit/Services/BaseTestCase.php
@@ -4,12 +4,14 @@ namespace Tests\Unit\Services;
 
 use Tests\TestCase;
 use Tests\Traits\MockClassMethod;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class BaseTestCase extends TestCase
 {
     use WithFaker;
+    use ProphecyTrait;
     use MockClassMethod;
     use DatabaseTransactions;
 
